996258de55ee6988c1e6b6cdcfc12bb29fc8abf0
ZZPot <you@example.com>
2016-10-12 12:36:13 +1200

    Initial

---
+++ FeatureDetector.cpp
@@ -0,0 +1,146 @@
+#include "FeatureDetector.h"
+#include <opencv2/imgproc.hpp>
+
+#pragma warning(disable: 4244 4267 4018)
+
+bool CheckFeatures(Obj2d* obj, type_condition condition, int features_to_check)
+{
+	bool res = true;
+	if(features_to_check & FEATURE_CHECK_WIDTH)
+		if (!(condition.rect[0].width <= obj->rect.width) || !(obj->rect.width <= condition.rect[1].width))
+			res = false;
+	if(features_to_check & FEATURE_CHECK_HEIGHT)
+		if (!(condition.rect[0].height <= obj->rect.height) || !(obj->rect.height <= condition.rect[1].height))
+			res = false;
+	double size_ratio = obj->r_rect.size.width / obj->r_rect.size.height;
+	if(size_ratio < 1)
+		size_ratio = 1 / size_ratio;
+	if(features_to_check & FEATURE_CHECK_SIZE_RATIO)
+		if(!(condition.size_ratio[0] <= size_ratio) || !(size_ratio <= condition.size_ratio[1]))
+			res = false;
+	return res;
+}
+void GetObj2d(Obj2d* obj)
+{
+	obj->rect = cv::boundingRect(obj->contours[0][0]);
+	obj->r_rect = cv::minAreaRect(obj->contours[0][0]);
+	cv::Mat img = cv::Mat::zeros(obj->rect.size(), CV_8UC1);
+	std::vector<cv::Scalar> colors = {cv::Scalar(255), cv::Scalar(0)};
+	DrawContours(obj->contours, colors, img, cv::Point(-obj->rect.x, -obj->rect.y));
+	obj->square = countNonZero(img);
+}
+std::vector<Obj2d> FindObjects(cv::Mat img, std::vector<type_condition> conditions, std::vector<int> features_to_check, int mode, int level_limit)
+{
+	std::vector<Obj2d> res;
+	std::vector<contour_type> contours;
+	std::vector<cv::Vec4i> hierarchy;
+	cv::findContours(img, contours, hierarchy, mode, cv::CHAIN_APPROX_NONE);
+	if(level_limit < 0)
+		level_limit = INT_MAX;
+	std::set<unsigned> banished;
+	for(unsigned i = 0; i < contours.size(); i++)
+	{
+		if(banished.find(i) != banished.end()) // preserve from double including parent and child contours
+			continue;
+		if(GetContourLevel(hierarchy, i) > level_limit)
+			continue;
+		Obj2d temp_obj;
+		temp_obj.contours = GetContours(GetContours(hierarchy, i), contours);
+		GetObj2d(&temp_obj);
+		for(unsigned j = 0; j < conditions.size(); j++)
+			if(CheckFeatures(&temp_obj, conditions[j], features_to_check[j]))
+			{
+				res.push_back(temp_obj);
+				BanishContour(banished, hierarchy, i);
+				break;
+			}
+		if(!conditions.size())
+		{
+			res.push_back(temp_obj);
+			BanishContour(banished, hierarchy, i);
+		}
+
+	}
+	return res;
+}
+void DrawContours(	std::vector<std::vector<contour_type>> contours,
+					std::vector<cv::Scalar> colors, cv::Mat& img, 
+					cv::Point offset, int level_limit)
+{
+	if (level_limit < 0)
+		level_limit = contours.size();
+	for(unsigned i = 0; i < level_limit; i++)
+	{
+		unsigned color_num = i % colors.size(); // cyclically switching colors
+		cv::drawContours(img, contours[i], -1, colors[color_num], CV_FILLED, cv::LINE_8, cv::noArray(), INT_MAX, offset);
+	}
+}
+int GetContourLevel(std::vector<cv::Vec4i> hierarchy, unsigned contour_num)
+{
+	int res = 0;
+	for(;hierarchy[contour_num][3] != -1; res++) // -1 means that contour hasn't parent
+		contour_num = hierarchy[contour_num][3]; 
+	return res;
+}
+std::vector<unsigned> GetChildren(std::vector<cv::Vec4i> hierarchy, unsigned contour_num)
+{
+	std::vector<unsigned> res;
+	for(unsigned i = 0; i < hierarchy.size(); i++)
+	{
+		if(hierarchy[i][3] == contour_num) // [i][3] is parent of i
+			res.push_back(i);
+	}
+	return res;
+}
+std::vector<std::vector<unsigned>> GetContours(std::vector<cv::Vec4i> hierarchy, unsigned contour_num)
+{
+	std::vector<std::vector<unsigned>> res;
+	std::vector<unsigned> current_contours;
+	current_contours.push_back(contour_num);
+	res.push_back(current_contours);
+	while(current_contours.size())
+	{
+		std::vector<unsigned> children_level; // all children at current level
+		for(auto current: current_contours)
+		{
+			std::vector<unsigned> children = GetChildren(hierarchy, current);
+			children_level.insert(children_level.end(), children.begin(), children.end());
+		}
+		res.push_back(children_level);
+		current_contours = children_level;
+	}
+	return res;
+}
+std::vector<std::vector<contour_type>> GetContours(std::vector<std::vector<unsigned>> contours_nums, std::vector<contour_type> contours)
+{
+	std::vector<std::vector<contour_type>> res;
+	for(auto level_nums: contours_nums)
+	{
+		std::vector<contour_type> level;
+		for(auto contour_num: level_nums)
+			level.push_back(contours[contour_num]);
+		res.push_back(level);
+	}
+	return res;
+}
+void BanishContour(std::set<unsigned>& banished, std::vector<cv::Vec4i> hierarchy, unsigned contour_num)
+{
+	std::vector<std::vector<unsigned>> contours = GetContours(hierarchy, contour_num);
+	for(auto level: contours)
+		for(auto contour: level)
+			banished.insert(contour);
+}
+void DrawRect(cv::Rect rect, cv::Mat& img, cv::Scalar color, int thickness)
+{
+	cv::rectangle(img, rect, color, thickness);
+}
+void DrawRRect(cv::RotatedRect r_rect, cv::Mat& img, cv::Scalar color, int thickness)
+{
+	cv::Point2f points_f[4];
+	r_rect.points(points_f);
+	std::vector<cv::Point> points = {points_f[0], points_f[1], points_f[2], points_f[3]};
+	if(thickness < 0)
+		cv::fillPoly(img, points, color);
+	else
+		cv::polylines(img, points, true, color, thickness);
+}
---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "FeatureDetector.h"
+
+using namespace cv;
+#define IMG_NAME	"1.jpg"
+#define WND_NAME_TEMP	""
+#define SHOW_N_WAIT(img) imshow(WND_NAME_TEMP, img); waitKey(0);
+
+Mat Binarize(Mat img);
+Scalar RandomColor(RNG& rng);
+int main()
+{
+	Mat input_img = imread(IMG_NAME, IMREAD_GRAYSCALE);
+		SHOW_N_WAIT(input_img);
+	Mat img_blur;
+	medianBlur(input_img, img_blur, 5);
+		SHOW_N_WAIT(img_blur);
+	Mat img_bin;
+	Canny(img_blur, img_bin, 50, 150);
+		SHOW_N_WAIT(img_bin);
+	img_bin = Binarize(img_bin);
+		SHOW_N_WAIT(img_bin);
+	morphologyEx(img_bin, img_bin, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, Size(5, 5))); // clean
+		SHOW_N_WAIT(img_bin);
+	std::vector<type_condition> cond;
+	cond.resize(1);
+	cond[0].size_ratio[0] = 1;
+	cond[0].size_ratio[1] = 7;
+	cond[0].rect[0].width = 20;
+	cond[0].rect[1].width = 300;
+	cond[0].rect[0].height = 20;
+	cond[0].rect[1].height = 300;
+	std::vector<int> check = {FEATURE_CHECK_SIZE | FEATURE_CHECK_SIZE_RATIO};
+	Mat img_obj = Mat::zeros(img_bin.size(), CV_8UC3);
+	std::vector<Obj2d> objects = FindObjects(img_bin, cond, check, RETR_EXTERNAL, 0);
+	std::vector<Scalar> colors = {Scalar::all(255), Scalar::all(0)};
+	RNG rng((unsigned)-1);
+	cvtColor(input_img, input_img, CV_GRAY2BGR);
+	for(auto& obj: objects)
+	{
+		//DrawContours(obj.contours, colors, img_obj, Point(), 1);
+		DrawRRect(obj.r_rect, input_img, RandomColor(rng));
+	}
+		SHOW_N_WAIT(input_img);
+	return 0;
+}
+
+Mat Binarize(Mat img)
+{
+	Mat res;
+	threshold(img, res, 0, 255, THRESH_OTSU);
+	return res;
+}
+Scalar RandomColor(RNG& rng)
+{
+	int icolor=(unsigned) rng;
+	return Scalar( icolor&255, (icolor>>8)&255, (icolor>>16)&255 );
+}
